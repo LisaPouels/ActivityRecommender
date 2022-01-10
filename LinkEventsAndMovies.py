31edc2ba65e172a03000c504fbc7936411165e63
Lisa Pouels <l.l.pouels@student.tue.nl>
2022-01-10 16:57:27 +0100

    Added functions into code

--- LinkEventsAndMovies.py
+++ LinkEventsAndMovies.py
@@ -4,59 +4,71 @@ import pandas as pd
 import numpy as np
 from collections import Counter
 
+def SplitCategories(category_data):
+
+    for i in range(0, len(category_data)):
+        category_list = category_data[i]
+        categories = category_list.split(', ')
+        category_data[i] = categories
+
+    return category_data
+
+def CatCount(cat_data):
+    all_categories = []
+    for i in cat_data:
+        for category in i:
+            all_categories.append(category)
+   
+    cats = Counter(all_categories).keys()
+    count = Counter(all_categories).values()
+
+    cat_count = pd.DataFrame()
+    cat_count['Category'] = cats
+    cat_count['Count'] = count
+
+    return cat_count
+
+def TopCats(cat_data, n):
+    cat_count = CatCount(cat_data)
+    sorted_cat_count = cat_count.sort_values(by=['Count'], ascending=False)
+    top_5 = sorted_cat_count.head(n)
+
+    return top_5
+
+def EventWeights(event_data, event_cat_data, top_cats):
+    event_weights = []
+    for i in event_cat_data:
+        weight = 0
+        for category in i:
+            for cat in top_cats['Category']:
+                if str(category) in cat:
+                    weight += 1
+            final_weight = weight/len(i)
+        event_weights.append(final_weight)
+
+    events_df = pd.DataFrame()
+    events_df['Event'] = event_data['Event']
+    events_df['Weights'] = event_weights
+
+    return events_df
+
+def RecommendEvents(event_data, event_cat_data, top_cats, n):
+    events_df = EventWeights(event_data, event_cat_data, top_cats)
+    sorted_events = events_df.sort_values(by=['Weights'], ascending=False)
+
+    recommended_events = sorted_events['Event'].head(n)
+
+    return recommended_events
+
 movie_1 = MovieInformation.movie_info_1.reset_index(drop=True)
-category_list = movie_1['listed_in']
-
-for i in range(0, len(category_list)):
-    categories = category_list[i]
-    parts = categories.split(', ')
-    category_list[i] = parts
-
+movie_categories = movie_1['listed_in']
+movie_cats_split = SplitCategories(movie_categories)
 
 events = CreateEvents.events_data
 event_cats = events['Category']
+event_cats_split = SplitCategories(event_cats)
 
-for i in range(0, len(event_cats)):
-    categories = event_cats[i]
-    parts = categories.split(', ')
-    event_cats[i] = parts
+top_5 = TopCats(movie_cats_split, 5)
 
-all_categories = []
-for category in category_list:
-    for item in category:
-        all_categories.append(item)
-        
-
-cats = Counter(all_categories).keys()
-count = Counter(all_categories).values()
-
-cat_count = pd.DataFrame()
-cat_count['Category'] = cats
-cat_count['Count'] = count
-
-percent_count = []
-for val in count:
-    percent_count.append((val/sum(count))*100)
-
-cat_count['Percentage'] = percent_count
-
-sorted_cat_count = cat_count.sort_values(by=['Percentage'], ascending=False)
-top_5 = sorted_cat_count.head()
-
-event_weights = []
-for i in event_cats:
-    weight = 0
-    for category in i:
-        for cat in top_5['Category']:
-            if str(category) in cat:
-                weight += 1
-        final_weight = weight/len(i)
-    event_weights.append(final_weight)
-
-events_df = pd.DataFrame()
-events_df['Event'] = events['Event']
-events_df['Weights'] = event_weights
-
-sorted_events = events_df.sort_values(by=['Weights'], ascending=False)
-
-print(sorted_events['Event'].head(3))
+recommended_events = RecommendEvents(events, event_cats_split, top_5, 3)
+print(recommended_events)
